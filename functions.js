ee5e21477b31f5f4a11a3216e26e0ad6ccd7f837
Cristian Barillas <cristianvx@yahoo.com>
2018-09-04 23:30:31 -0600

    some actions

--- functions.js
+++ functions.js
@@ -1,3 +1,11 @@
 var sections = $('section')
     , nav = $('nav')
     , nav_height = nav.outerHeight();
+
+    $(document).ready(function() {
+        $("#resumen").remove();
+    
+        $("#target1").parent().css("background-color","red");
+        $(".target:even").css("background","gray");//aplica la funcion a los objetos impares de la clase 'target'
+        $(".well:odd").css("background", "purple");//aplica la funcion a los objetos pares de la clase 'well'
+    });
